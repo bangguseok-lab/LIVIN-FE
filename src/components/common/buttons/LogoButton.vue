e8178aec26a57819ee7ee29714d40ee65c474ce0
조아빈 <whdkqls122@naver.com>
2025-07-21 20:43:24 +0900

    ✨ Feat : 헤더바 로고 버튼 추가 - 전달 받은 props에 따른 아이콘 색이 출력되도록 구현

---
+++ src/components/common/buttons/LogoButton.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+const props = defineProps({
+  icon: {
+    Type: String,
+    required: true,
+  },
+})
+const router = useRouter()
+const home = () => {
+  router.push('/')
+}
+</script>
+<template>
+  <div class="logo-button-wrap" @click.prevent="home">
+    <img
+      src="../../../assets/icons/header/LogoBlue.svg"
+      alt="로고 파란색"
+      v-if="props.icon === 'blue'"
+    />
+    <img
+      src="../../../assets/icons/header/LogoWhite.svg"
+      alt="로고 하얀색"
+      v-else-if="props.icon === 'white'"
+    />
+  </div>
+</template>
+<style scoped>
+.logo-button-wrap {
+  width: 30%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
