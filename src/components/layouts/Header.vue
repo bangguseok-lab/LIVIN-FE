84bdd7c83e5bedd213fbc9932a5904da66db0f89
조아빈 <whdkqls122@naver.com>
2025-07-21 21:12:22 +0900

    ✨ Feat: 상단 헤더바 구현 - 경로 이동에 따른 헤더바 형태 및 색 변경하도록 구현 - 화면 상단에 항상 고정하도록 구현

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,11 +1,72 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup></script>
+<script setup>
+import BackButton from '@/components/common/buttons/BackButton.vue'
+import LogoButton from '@/components/common/buttons/LogoButton.vue'
+import { computed } from 'vue'
+
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+})
+
+const background = computed(() => {
+  if (props.icon === 'blue') {
+    return 'background-white'
+  } else {
+    return 'background-blue'
+  }
+})
+</script>
 
 <template>
-  <div class="Header"></div>
+  <div v-if="props.type === 'together'" class="Header" :class="background">
+    <BackButton :icon="props.icon" />
+    <LogoButton :icon="props.icon" />
+    <div class="header-right"></div>
+  </div>
+  <div
+    v-else-if="props.type === 'alone-logo'"
+    class="Header-alone"
+    :class="background"
+  >
+    <LogoButton :icon="props.icon" />
+  </div>
 </template>
 
 <style scoped>
+.Header,
+.Header-alone {
+  width: 100%;
+  height: 3rem;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+}
 .Header {
+  justify-content: space-between;
+}
+.Header-alone {
+  justify-content: flex-start;
+}
+.header-right {
+  width: 10%;
+  height: 100%;
+}
+.background-blue {
+  background-color: #177dfa;
+}
+.background-white {
+  background-color: #ffffff;
 }
 </style>
