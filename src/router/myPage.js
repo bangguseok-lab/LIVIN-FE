4ab5f62f3bfafb6fd42cad96d0f378308af01768
ll-04 <dmstkdsnsk@naver.com>
2025-07-22 11:02:25 +0900

    ✨ Feat: 마이페이지 기본 레이아웃과 사용자 정보 수정, 타입 토글 기능 구현 #3
    - 마이페이지 전체 UI 레이아웃 및 섹션 구성 완료
    
    - 사용자 정보(이름, 닉네임, 생년월일, 연락처) 표시 및 닉네임/연락처 수정 기능 구현
    
    - 임대인/임차인 타입 토글 버튼 추가 및 선택에 따른 관리 버튼 텍스트 동적 변경
    
    - 수정 시작/완료 버튼 토글로 편집 상태 제어 및 내용 저장 처리

--- src/router/myPage.js
+++ src/router/myPage.js
@@ -1,1 +1,9 @@
-export default []
+import MyPage from '@/pages/myPage/MyPage.vue' // 경로는 실제 위치에 따라 조정!
+
+export default [
+  {
+    path: '/mypage',
+    name: 'MyPage',
+    component: MyPage,
+  },
+]
