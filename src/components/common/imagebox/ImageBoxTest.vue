4554623f6e7fee54d6edcb01b223a125459c0c2e
ll-04 <dmstkdsnsk@naver.com>
2025-07-24 21:42:35 +0900

    🧪 Test: 프로필 이미지 틀 테스트 png 파일로 변경

--- src/components/common/imagebox/ImageBoxTest.vue
+++ src/components/common/imagebox/ImageBoxTest.vue
@@ -1,29 +1,36 @@
 <script setup>
 import { ref } from 'vue'
 import ImageBox from '@/components/common/imagebox/ImageBox.vue'
-import profileSvgUrl from '@/assets/images/profile/test-img.svg?url'
 
-const images = [profileSvgUrl, profileSvgUrl, profileSvgUrl]
+// ✅ 색상 다른 PNG 이미지 3개 import
+import img1 from '@/assets/images/profile/test1.png'
+import img2 from '@/assets/images/profile/test2.png'
+import img3 from '@/assets/images/profile/test3.png'
+
+const images = [img1, img2, img3]
+
+// 프로필 이미지 선택 상태
 const selectedIndex = ref(null)
-
 const handleSelect = index => {
   selectedIndex.value = index
 }
 </script>
 
 <template>
-  <div style="padding: 60px">
-    <h2 style="font-size: large">프로필 이미지 테스트</h2>
-    <div style="display: flex; gap: 16px">
-      <ImageBox
-        v-for="(img, idx) in images"
-        :key="idx"
-        :image="img"
-        type="profile"
-        :selected="selectedIndex === idx"
-        @select="() => handleSelect(idx)"
-      />
-    </div>
+  <div style="padding: 60px; display: flex; flex-direction: column; gap: 40px">
+    <section>
+      <h2 style="font-size: large; text-align: center">프로필 이미지 테스트</h2>
+      <div style="display: flex; gap: 30px">
+        <ImageBox
+          v-for="(img, idx) in images"
+          :key="'profile-' + idx"
+          :image="img"
+          type="profile"
+          :selected="selectedIndex === idx"
+          @select="() => handleSelect(idx)"
+        />
+      </div>
+    </section>
   </div>
 </template>
 
