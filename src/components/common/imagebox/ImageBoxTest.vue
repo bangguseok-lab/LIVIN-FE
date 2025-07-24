8504f70536a668f439a673d05b89378c46ca25c0
ll-04 <dmstkdsnsk@naver.com>
2025-07-24 16:14:16 +0900

    ✨ Feat: 프로필 이미지 틀  구현 #41
    - 회원가입/마이페이지 프로필 설정 이미지 틀 구현
    - 이미지 틀 테스트 페이지 구현
    - 컴포넌트 라우터 연결

---
+++ src/components/common/imagebox/ImageBoxTest.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import ImageBox from '@/components/common/imagebox/ImageBox.vue'
+import profileSvgUrl from '@/assets/images/profile/test-img.svg?url'
+
+const images = [profileSvgUrl, profileSvgUrl, profileSvgUrl]
+const selectedIndex = ref(null)
+
+const handleSelect = index => {
+  selectedIndex.value = index
+}
+</script>
+
+<template>
+  <div style="padding: 60px">
+    <h2 style="font-size: large">프로필 이미지 테스트</h2>
+    <div style="display: flex; gap: 16px">
+      <ImageBox
+        v-for="(img, idx) in images"
+        :key="idx"
+        :image="img"
+        :selected="selectedIndex === idx"
+        @select="() => handleSelect(idx)"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/components.js
+++ src/router/components.js
@@ -1,12 +1,19 @@
 // router/dev.js
 import ButtonsView from '../components/common/buttons/ButtonsView.vue'
 import InputsView from '@/components/common/input/InputsView.vue'
+import ImageBoxTest from '@/components/common/imagebox/ImageBoxTest.vue'
 
 const devRoutes = [
   { path: '/testButton', name: 'testButton', component: ButtonsView },
 
   // 다른 테스트 컴포넌트들도 여기에 추가 가능
   { path: '/testInput', name: 'testInput', component: InputsView },
+
+  {
+    path: '/test/image-box',
+    name: 'ImageBoxTest',
+    component: ImageBoxTest,
+  },
 ]
 
 export default devRoutes
