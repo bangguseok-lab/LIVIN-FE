e834ef457d51e7df5f82670ab822dcfe5b562ccd
Youngbae1126 <devhyun1126@gmail.com>
2025-07-26 01:59:39 +0900

    :recycle: Refactor: 카카오 요청 시 providerId 파라미터에 추가될 수 있도록 수정

--- src/pages/auth/KakaoCallback.vue
+++ src/pages/auth/KakaoCallback.vue
@@ -16,14 +16,15 @@ onMounted(async () => {
   if (code) {
     try {
       const res = await axios.get(`/api/kakao/callback?code=${code}`)   // 토큰 발급
-
-      store.setProviderId(res.data)
+      const providerId = res.data
+      store.setProviderId(providerId)
 
       router.push('/home')    // 로그인 성공
     } catch (err) {
       if (err.response?.status === 404) {
         // 회원정보가 없는 경우, 회원가입 진행
-        const providerId = store.providerId
+        const providerId = err.response.data
+        store.setProviderId(providerId)
         router.push(`/auth/signup?providerId=${providerId}`)
       } else {
         console.error('카카오 로그인 실패', err)
