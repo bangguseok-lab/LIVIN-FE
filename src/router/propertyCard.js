d8ea6de6b1e1e1aab0e2b7f1fa0709a38625ba88
imnjb@naver.com <imnjb@naver.com>
2025-08-04 08:11:57 +0900

    라우터 경로 수정

--- src/router/propertyCard.js
+++ src/router/propertyCard.js
@@ -2,6 +2,6 @@ export default [
   {
     path: '/cards',
     name: 'propertycard',
-    component: () => import('@/pages/cards/PropertyCardView.vue'),
+    component: () => import('@/components/cards/PropertyCardView.vue'),
   },
 ]
