2701fb94c6bdd9bacc26c7b956a6666487439062
Youngbae1126 <devhyun1126@gmail.com>
2025-08-06 14:05:54 +0900

    :bug: Fix: 닉네임 조회 안 되는 문제 해결 - 수정된 엔드포인트 반영

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import apiClient from './apiClient'
 
 export default {
   async getNickname() {
-    const { data } = await apiClient.get('/users')
+    const { data } = await apiClient.get('/users/nickname')
     return data.nickname
   },
 }
