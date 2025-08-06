771d44bc40d610d26be952fff9e63a56b5605fdb
jjang0308 <jang653838@naver.com>
2025-08-06 15:34:06 +0900

    :truck: Route : 체크리스트 router 설정

--- src/router/checklist.js
+++ src/router/checklist.js
@@ -8,11 +8,12 @@ export default [
     path: '/checklist/:id',
     name: 'checklistDetail',
     component: () => import('@/pages/checklist/ChecklistDetail.vue'),
-    props: true,
+    // props: true,
   },
   {
     path: '/checklist/create',
     name: 'checklistCreate',
     component: () => import('@/pages/checklist/ChecklistAdd.vue'),
+    props: true,
   },
 ]
