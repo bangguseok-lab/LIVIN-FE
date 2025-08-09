d069615a49bf9c57919844fa51465f12db62f655
KyungLim11 <grkang3112@gmail.com>
2025-08-09 15:42:51 +0900

    :sparkles: Feat: 필터링 활성 상태 로직 추가
    - 패널이 열려있으면 항상 활성화
    - 선택된 필터링 값이 존재하면 활성화
    - 이 외의 경우 필터링 버튼 비활성화
    - 위 로직 구현

--- src/components/filters/sections/FilterDropdownSection.vue
+++ src/components/filters/sections/FilterDropdownSection.vue
@@ -4,6 +4,7 @@ import FilterButton from '@/components/filters/FilterButton.vue'
 import DealTypePanel from '@/components/panels/DealTypePanel.vue'
 import RegionPanel from '@/components/panels/RegionPanel.vue'
 import PricePanel from '@/components/panels/PricePanel.vue'
+import { usePriceStore } from '@/stores/priceStore'
 
 const props = defineProps({
   mode: String,
@@ -11,6 +12,38 @@ const props = defineProps({
   regionData: Object,
   region: Object,
 })
+
+const priceStore = usePriceStore()
+
+// 선택 상태 계산
+const isDealSelected = computed(
+  () => Array.isArray(props.dealType) && props.dealType.length > 0,
+)
+
+const isRegionSelected = computed(() => {
+  const r = props.region || {}
+  return !!(r.city || r.district || r.parish)
+})
+
+const isPriceSelected = computed(() => {
+  const { jeonseDeposit, monthlyDeposit, monthlyRent } = priceStore.states
+  const has = r => r && (r.min !== null || r.max !== null)
+  return has(jeonseDeposit) || has(monthlyDeposit) || has(monthlyRent)
+})
+
+function isButtonActive(panelKey) {
+  if (activePanel.value === panelKey) return true
+  switch (panelKey) {
+    case 'deal':
+      return isDealSelected.value
+    case 'region':
+      return isRegionSelected.value
+    case 'price':
+      return isPriceSelected.value
+    default:
+      return false
+  }
+}
 
 // 각 패널에서 선택된 값을 v-model 스타일로 상위에 올려줌
 const emit = defineEmits([
@@ -109,19 +142,19 @@ const currentPanelComponent = computed(() => {
       <FilterButton
         label="거래 유형"
         panelKey="deal"
-        :is-active="activePanel === 'deal'"
+        :is-active="isButtonActive('deal')"
         @click="e => togglePanel(e, 'deal')"
       />
       <FilterButton
         label="지역"
         panelKey="region"
-        :is-active="activePanel === 'region'"
+        :is-active="isButtonActive('region')"
         @click="e => togglePanel(e, 'region')"
       />
       <FilterButton
         label="가격"
         panelKey="price"
-        :is-active="activePanel === 'price'"
+        :is-active="isButtonActive('price')"
         @click="e => togglePanel(e, 'price')"
       />
     </div>
