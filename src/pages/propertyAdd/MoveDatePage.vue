632d335663b6ff04336ce4e9b0c153c12f492565
Youngbae1126 <devhyun1126@gmail.com>
2025-08-12 04:54:35 +0900

    :sparkles: Feat: 매물 등록 관련 모든 페이지 기본 레이아웃 마크업, 스타일 및 라우터 설정 완료

---
+++ src/pages/propertyAdd/MoveDatePage.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import Buttons from '@/components/common/buttons/Buttons.vue';
+
+const router = useRouter()
+
+const handlePrevClick = () => {
+  router.push({ name: "optionPage" })
+}
+
+const handleNextClick = () => {
+  router.push({ name: "lastPage" })
+}
+</script>
+
+<template>
+  <div class="MoveDatePage">
+    <div class="moveDate-container"></div>
+    <div class="button-wrapper">
+      <Buttons type="default" label="이전" @click="handlePrevClick" class="prevBtn" />
+      <Buttons type="default" label="다음" @click="handleNextClick" class="nextBtn" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.MoveDatePage {
+  position: relative;
+  width: 100%;
+  height: 90%;
+}
+
+.moveDate-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 70%;
+}
+
+.button-wrapper {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 2rem;
+  padding-top: 4rem;
+}
+
+.prevBtn,
+.nextBtn {
+  width: 100%;
+  height: rem(50px);
+  margin-bottom: 5rem;
+}
+</style>
---
+++ src/pages/propertyAdd/RoomDirectionPage.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import Buttons from '@/components/common/buttons/Buttons.vue';
+
+const router = useRouter()
+
+const handlePrevClick = () => {
+  router.push({ name: "photoPage" })
+}
+
+const handleNextClick = () => {
+  router.push({ name: "roomDetailPage" })
+}
+</script>
+
+<template>
+  <div class="RoomDirectionPage">
+    <div class="roomDirection-container"></div>
+    <div class="button-wrapper">
+      <Buttons type="default" label="이전" @click="handlePrevClick" class="prevBtn" />
+      <Buttons type="default" label="다음" @click="handleNextClick" class="nextBtn" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.RoomDirectionPage {
+  position: relative;
+  width: 100%;
+  height: 90%;
+}
+
+.roomDirection-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 70%;
+}
+
+.button-wrapper {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 2rem;
+  padding-top: 4rem;
+}
+
+.prevBtn,
+.nextBtn {
+  width: 100%;
+  height: rem(50px);
+  margin-bottom: 5rem;
+}
+</style>
