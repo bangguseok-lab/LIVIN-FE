4703789c7aa4577efb11f2a5dde9e698dafb06f6
Youngbae1126 <devhyun1126@gmail.com>
2025-08-18 11:20:21 +0900

    :bug: Fix: type 선택 페이지 유효성 검사 추가

--- src/pages/propertyAdd/PropertyTypePage.vue
+++ src/pages/propertyAdd/PropertyTypePage.vue
@@ -27,33 +27,28 @@ const onMonthlyRentClick = () => {
 
 // 다음 페이지로 이동
 const handleClick = () => {
-  // 부동산 고유번호가 비어있지 않을 때
-  if (jeonseOptIsActive.value) {
-    propertyStore.updateNewProperty('transactionType', 'JEONSE')
-    router.push({ name: 'jeonsePage' })
+  // 아무것도 선택하지 않고 넘어가려고 할 때, 유효성 검사 처리
+  if (!jeonseOptIsActive.value && !monthlyRentOptIsActive.value) {
+    alert('거래 유형을 선택해주세요')
   } else {
-    propertyStore.updateNewProperty('transactionType', 'MONTHLY_RENT')
-    router.push({ name: 'wolsePage' })
+    // 부동산 고유번호가 비어있지 않을 때
+    if (jeonseOptIsActive.value) {
+      propertyStore.updateNewProperty('transactionType', 'JEONSE')
+      router.push({ name: 'jeonsePage' })
+    } else {
+      propertyStore.updateNewProperty('transactionType', 'MONTHLY_RENT')
+      router.push({ name: 'wolsePage' })
+    }
   }
+
 }
 </script>
 
 <template>
   <div class="PropertyTypePage">
-    <Buttons
-      type="option"
-      label="전세"
-      v-model:isActive="jeonseOptIsActive"
-      class="type-button"
-      @click="onJeonseClick"
-    />
-    <Buttons
-      type="option"
-      label="월세"
-      v-model:isActive="monthlyRentOptIsActive"
-      class="type-button"
-      @click="onMonthlyRentClick"
-    />
+    <Buttons type="option" label="전세" v-model:isActive="jeonseOptIsActive" class="type-button" @click="onJeonseClick" />
+    <Buttons type="option" label="월세" v-model:isActive="monthlyRentOptIsActive" class="type-button"
+      @click="onMonthlyRentClick" />
   </div>
   <Buttons type="default" label="다음" @click="handleClick" class="nextBtn" />
 </template>
